cbfa6cf01360c88af8d1bca800f05ece5811fa47
sahil-777 <turbothunderboltz@gmail.com>
2021-01-02 20:35:24 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
-const mysql = require('mysql');
+const bodyParser = require('body-parser');
+var app= express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+const PORT =process.env.PORT || 3000 ;
+  
+app.listen(PORT,(err) =>{
+    if(err) throw err;
+    console.log('Server is running on localhost:'+PORT);
+});
