8895715416fedd1fd9f0bbd5dd2487f432059865
sahil-777 <turbothunderboltz@gmail.com>
2021-01-05 04:06:40 +0530

    Export function is yet to be done

--- model/createDb.js
+++ model/createDb.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql');
 const connection  = require('../config.js');
 
-connection.query("CREATE DATABASE IF NOT EXISTS socialnetwork DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; USE socialnetwork", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-});
+const createDatabase = () => {
+    connection.query("CREATE DATABASE IF NOT EXISTS socialnetwork DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; USE socialnetwork", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+};
 
-module.exports = connection;
+//exports.createDatabase = createDatabase;
+createDatabase();
