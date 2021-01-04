e81b6de550abcb3f2e5dad222902860c3cffbc4b
sahil-777 <turbothunderboltz@gmail.com>
2021-01-05 02:14:49 +0530

    Moved Db create query to model/createDb.js

--- config.js
+++ config.js
@@ -10,10 +10,6 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  connection.query("CREATE DATABASE IF NOT EXISTS socialnetwork DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; USE socialnetwork", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
 });
 
 module.exports = connection;
