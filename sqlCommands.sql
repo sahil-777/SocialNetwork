ad64d6067ccfeb1b9d64140fd5716c315a6a8047
sahil-777 <turbothunderboltz@gmail.com>
2021-01-18 03:00:35 +0530

    Testing

--- sqlCommands.sql
+++ sqlCommands.sql
@@ -10,3 +10,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Demo Insert --
+INSERT INTO users (username, password, email, created_at) VALUES ('Company Inc', 'Highway 37','s@gmail.com',STR_TO_DATE('12/04/2016 15:30:35','%d/%m/%Y %H:%i:%s'));
+
+SELECT * FROM users WHERE username='SahilVelhal' AND password='dsg';
