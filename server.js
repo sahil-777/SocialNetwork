e4b5eafc403d91a97ccd2fa6bd7924e74a8d99e2
sahil-777 <turbothunderboltz@gmail.com>
2021-01-18 02:57:38 +0530

    Added Login path,function

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get('/signup',(req,res)=>{
     res.render('signupView');
 });
 
-
 app.post('/signup',(req,res)=>{
     var data ={
         "username":req.body.userName,
@@ -30,6 +29,62 @@ app.post('/signup',(req,res)=>{
     connection.query('INSERT INTO users SET ?',data);
     res.redirect('/');
 });
+
+
+app.get('/login',(req,res)=>{
+    res.render('loginView',{msg:null});
+});
+
+app.post('/login',(req,res)=>{
+    var data ={
+        "username":req.body.userName,
+        "password":req.body.passWord,
+    };
+    console.log(data);
+
+    connection.query('SELECT * FROM users WHERE username = ?',data.username,function (error, results, fields) {
+        if (error) {
+          res.send({
+            "code":400,
+            "failed":"error ocurred"
+          })
+        }else{
+          if(results.length >0){
+            const comparision = data.password==results[0].password? true:false;
+            if(comparision){
+                /*res.send({
+                  "code":200,
+                  "success":"login sucessfull"
+                })*/
+                console.log('Login Successful');
+                res.redirect('/');
+            }
+            else{
+              /*res.send({
+                   "code":204,
+                   "success":"Email and password does not match"
+              })*/
+              console.log('Username and password does not match');
+              msg="Username and password does not match";
+              res.render('loginView',{msg:msg});
+            }
+          }
+          else{
+            /*res.send({
+              "code":206,
+              "success":"Email does not exits"
+                });*/
+                console.log('Username does not exists');
+                msg="Username does not exists";
+                res.render('loginView',{msg:msg});
+            }
+        }
+        });
+
+    
+});
+
+
 
 app.listen(PORT,(err) =>{
     if(err) throw err;
