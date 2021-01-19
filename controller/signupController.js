0660785e69643586af61f052f4d49fce236beeee
sahil-777 <turbothunderboltz@gmail.com>
2021-01-20 04:29:42 +0530

    Added: Controller for signup

---
+++ controller/signupController.js
@@ -0,0 +1,23 @@
+const connection = require('../config');
+
+class signupController{
+    displayPage (req,res){
+        return res.render('signupView',{msg:null});
+    }
+
+    enterInfo (req,res){
+        var data ={
+            "username":req.body.userName,
+            "password":req.body.passWord,
+            "email":req.body.email,
+            //"created_at":new Date().toLocaleString().slice(0, 19).replace('T', ' ')
+            //created_at: MySQL will take default value automatically
+        };
+        console.log(data);
+        connection.query('INSERT INTO users SET ?',data);
+        res.redirect('/');
+    } 
+}
+
+module.exports = signupController;
+
