b38dc077f58756da12d8d67f0eab993fe48daa07
sahil-777 <turbothunderboltz@gmail.com>
2021-01-20 18:04:51 +0530

    Added: loginController

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,55 +1,14 @@
 const connection = require('../config');
-
+const Model = require('../model/loginModel');
+const loginModel = new Model();
 class loginController{
     displayPage (req,res){
         return res.render('loginView',{msg:null});
     }
 
     enterInfo (req,res){
-    var data ={
-        "username":req.body.userName,
-        "password":req.body.passWord,
-    };
-    console.log(data);
-
-    connection.query('SELECT * FROM users WHERE username = ?',data.username,function (error, results, fields) {
-        if (error) {
-          res.send({
-            "code":400,
-            "failed":"error ocurred"
-          })
-        }else{
-          if(results.length >0){
-            const comparision = data.password==results[0].password? true:false;
-            if(comparision){
-                /*res.send({
-                  "code":200,
-                  "success":"login sucessfull"
-                })*/
-                console.log('Login Successful');
-                return res.redirect('/');
-            }
-            else{
-              /*res.send({
-                   "code":204,
-                   "success":"Email and password does not match"
-              })*/
-              console.log('Username and password do not match');
-              var msg="Username and password do not match";
-              return res.render('loginView',{msg:msg});
-            }
-          }
-          else{
-            /*res.send({
-              "code":206,
-              "success":"Email does not exits"
-                });*/
-                console.log('Username does not exists');
-                var msg="Username does not exists";
-                return res.render('loginView',{msg:msg});
-            }
-        }
-        });
+        loginModel.enterInfo(req,res);
+        //console.log("contMsg=> "+msg);
     } 
 
 }
