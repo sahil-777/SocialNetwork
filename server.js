31e91e70a33e4b9bc5f16eaf4743b7a41b8c679f
sahil-777 <turbothunderboltz@gmail.com>
2021-01-23 04:55:12 +0530

    Added:  express-session

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-var app= express();
+var app = express();
 //app.use(express.static(__dirname + "/public")); // all statics files in /public
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 const PORT =process.env.PORT || 3000 ;
 
+//SESSION
+const session = require('express-session');
+app.use(session({secret: 'Secreyyyy', resave: true, saveUninitialized: true})) 
+module.exports.session = session; //Unnecessary
 
 const loginRoute=require('./routes/loginRoute');
 app.use('/',loginRoute);
