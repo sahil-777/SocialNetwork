25ebebe654e5a4200ead7f4f2d5397dab2854dd5
sahil-777 <turbothunderboltz@gmail.com>
2021-01-22 21:24:29 +0530

    Updated: Session is yet to be implemented

--- model/loginModel.js
+++ model/loginModel.js
@@ -1,6 +1,8 @@
 const connection = require('../config');
-class loginModel {
-     enterInfo(req,res){
+
+class loginModel {   
+  enterInfo(req,res){
+
             var data ={
                 "username":req.body.userName,
                 "password":req.body.passWord,
@@ -17,40 +19,22 @@ class loginModel {
                   if(results.length >0){
                     const comparision = data.password==results[0].password? true:false;
                     if(comparision){
-                        /*res.send({
-                          "code":200,
-                          "success":"login sucessfull"
-                        })*/
-                        //console.log('Login Successful');
                         var msg=null;
-                        //console.log("* "+msg);
+                       
                         return res.render('homeView',{msg:msg});
                         
                     }
                     else{
-                      /*res.send({
-                           "code":204,
-                           "success":"Email and password does not match"
-                      })*/
-                      //console.log('Username and password do not match');
-                      var msg="Username and password do not match";
-                      //console.log("* "+msg);
-                      return res.render('loginView',{msg:msg});
+                       var msg="Username and password do not match";
+                       return res.render('loginView',{msg:msg});
                     }
                   }
                   else{
-                    /*res.send({
-                      "code":206,
-                      "success":"Email does not exits"
-                        });*/
-                        //console.log('Username does not exists');
-                        var msg="Username does not exists";
-                       // console.log("* "+msg);
+                    var msg="Username does not exists";
                         return res.render('loginView',{msg:msg});
                     }
                 }
             });
           }
   }
-
 module.exports = loginModel;
