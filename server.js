96c1d53f7d48062185bac74d902b8c9fb3c25764
sahil-777 <turbothunderboltz@gmail.com>
2021-01-28 19:55:28 +0530

    Added:Path to make editProfile POST request

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 const PORT =process.env.PORT || 3000 ;
 
+//Database
+const connection = require('./config');
+
 //SESSION
 const session = require('express-session');
 app.use(session({secret: 'Secreyyyy', resave: true, saveUninitialized: true})) 
@@ -27,6 +30,21 @@ app.use('/',homeRoute);
 
 const profileEditRoute=require('./routes/editProfileRoute');
 app.use('/profile',profileEditRoute);
+
+app.post('/profile/edit',(req,res)=>{
+    //res.send("Hii");
+    var data = {
+        "username":req.body.userName,
+        "profilepic":req.body.profilePic,
+        "fullname":req.body.name,
+        "birthdate":req.body.DOB,
+        "bio":req.body.Bio
+    };
+    res.send(data);
+    //You have to update (not insert)
+    //connection.query('INSERT INTO userinfo SET ?',data);
+
+});
 
 app.listen(PORT,(err) =>{
     if(err) throw err;
