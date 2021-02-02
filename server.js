143e333b8bb291471d996838fbf5bae98bd7fddc
sahil-777 <turbothunderboltz@gmail.com>
2021-02-02 15:51:03 +0530

    Updated, but MVC is yet to be added

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+const multer = require('multer'); 
 var app = express();
+
+var upload = multer({ dest: __dirname+ '/public/static/images/profilePics' });
+//console.log(upload);
+
 app.use(express.static(__dirname + "/public")); // all statics files in /public
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
@@ -32,37 +37,45 @@ app.use('/',homeRoute);
 const profileEditRoute=require('./routes/editProfileRoute');
 app.use('/profile',profileEditRoute);
 
-//IMP: Updated photo is not in a form as needed.
 
-app.post('/profile/edit',(req,res)=>{
-    //res.send("Hii");
+app.post('/profile/edit',upload.single('profilePic'),(req,res)=>{
+    console.log(req.file);
+    console.log(req.body);
 
-    connection.query('SELECT * FROM userinfo WHERE id=?',req.session.num,(error,userInfoResult)=>{
-        //console.log(userInfoResult[0]);
+    connection.query('SELECT * FROM userinfo WHERE id=4',req.session.num,(error,userInfoResult)=>{
+       if(error) throw error;
         var userInfo = {
             "username":(req.body.userName=='')?userInfoResult[0].username:req.body.userName,
-            "profilepic":(req.body.profilePic=='')?userInfoResult[0].profilepic:req.body.profilePic,
+            "profilepic":(!req.file)?userInfoResult[0].profilepic:req.file.filename,
             "fullname":(req.body.fullName=='')?userInfoResult[0].fullname:req.body.fullName,
             "birthdate":(req.body.DOB=='')?userInfoResult[0].birthdate:req.body.DOB,
             "bio":(req.body.Bio=='')?userInfoResult[0].bio:req.body.Bio,
-            //"id":req.session.num,
         };
-        console.log(userInfo);
-        //You have to update (not insert)
+    
         var sqlQuery="UPDATE userinfo SET ? WHERE id="+req.session.num;
         connection.query(sqlQuery,userInfo,(error,result)=>{
             if(error) throw error;
-            //console.log(result);
-            console.log(result.affectedRows+" record(s) updated");
+            //console.log(result.affectedRows+" record(s) updated");
             res.render('editProfileView',{msg:"Saved successfully!"});
         });
-        //res.send(userInfo);
-        
-        //res.send(userInfoResult);
     
     });
     
     
+    
+});
+
+app.get('/show',(req,res)=>{
+    /*var sql="SELECT profilepic FROM userinfo WHERE id = "+req.session.num;
+    connection.query(sql,(error,result)=>{
+        if(error) throw error;
+        console.log(result[0]);
+        //var imgg=new Buffer().toString('base64');
+        res.render('tempView',{profilePic:result[0]}); 
+
+    });*/
+    var pic=fs.readFileSync('./public/static/images/default_profilepic.png');
+    res.render('tempView',{profilePic:pic});
 });
 
 app.listen(PORT,(err) =>{
