2656e48756c508a674ba90f3c5294640143a733a
sahil-777 <turbothunderboltz@gmail.com>
2021-02-04 21:47:03 +0530

    Updated: /profile/edit & /profile, MVC yet to be added

--- server.js
+++ server.js
@@ -5,15 +5,35 @@ const fs = require('fs');
 const multer = require('multer'); 
 var app = express();
 
-var upload = multer({ dest: __dirname+ '/public/static/images/profilePics' });
+//var upload = multer({ dest: __dirname+ '/public' });
 //console.log(upload);
+//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/uploads" ) );
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,'uploads/profilepics/');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-app.use(express.static(__dirname + "/public")); // all statics files in /public
+
+ // all statics files in /public
 app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
-const PORT =process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000 ;
 
 //Database
 const connection = require('./config');
@@ -28,8 +48,9 @@ app.use('/',loginRoute);
 const signupRoute=require('./routes/signupRoute');
 app.use('/',signupRoute);
 
-const profileRoute=require('./routes/profileRoute');
-app.use('/',profileRoute);
+// /profile
+//const profileRoute=require('./routes/profileRoute');
+//app.use('/',profileRoute);
 
 const homeRoute=require('./routes/homeRoute');
 app.use('/',homeRoute);
@@ -42,7 +63,7 @@ app.post('/profile/edit',upload.single('profilePic'),(req,res)=>{
     console.log(req.file);
     console.log(req.body);
 
-    connection.query('SELECT * FROM userinfo WHERE id=4',req.session.num,(error,userInfoResult)=>{
+    connection.query('SELECT * FROM userinfo WHERE id=?',req.session.num,(error,userInfoResult)=>{
        if(error) throw error;
         var userInfo = {
             "username":(req.body.userName=='')?userInfoResult[0].username:req.body.userName,
@@ -52,12 +73,17 @@ app.post('/profile/edit',upload.single('profilePic'),(req,res)=>{
             "bio":(req.body.Bio=='')?userInfoResult[0].bio:req.body.Bio,
         };
     
-        var sqlQuery="UPDATE userinfo SET ? WHERE id="+req.session.num;
-        connection.query(sqlQuery,userInfo,(error,result)=>{
+        var sqlQuery1="UPDATE userinfo SET ? WHERE id="+req.session.num;
+        var sqlQuery2="UPDATE users SET username=? WHERE id="+req.session.num;
+        
+        connection.query(sqlQuery2,userInfo.username,(error,reslt)=>{
             if(error) throw error;
-            //console.log(result.affectedRows+" record(s) updated");
-            res.render('editProfileView',{msg:"Saved successfully!"});
+            connection.query(sqlQuery1,userInfo,(error,result)=>{
+                if(error) throw error;
+                res.render('editProfileView',{msg:"Saved successfully!"});
+            });
         });
+        
     
     });
     
@@ -65,17 +91,16 @@ app.post('/profile/edit',upload.single('profilePic'),(req,res)=>{
     
 });
 
-app.get('/show',(req,res)=>{
-    /*var sql="SELECT profilepic FROM userinfo WHERE id = "+req.session.num;
+app.get('/profile',(req,res)=>{
+    var sql="SELECT * FROM userinfo WHERE id = "+req.session.num;
     connection.query(sql,(error,result)=>{
         if(error) throw error;
         console.log(result[0]);
-        //var imgg=new Buffer().toString('base64');
-        res.render('tempView',{profilePic:result[0]}); 
-
-    });*/
-    var pic=fs.readFileSync('./public/static/images/default_profilepic.png');
-    res.render('tempView',{profilePic:pic});
+        var imageName=result[0].profilepic;
+        console.log(imageName);
+        if(typeof imageName=='object'){ imageName = "default_profilepic.png"; }
+        res.render('profileView',{data:result[0],imageName:imageName});
+    });
 });
 
 app.listen(PORT,(err) =>{
