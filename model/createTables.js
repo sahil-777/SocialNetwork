c9cb27d05d59ecbd27e5b925385ae61af5af4c0e
sahil-777 <turbothunderboltz@gmail.com>
2021-02-05 01:05:51 +0530

    Added:create userinfo table query

--- model/createTables.js
+++ model/createTables.js
@@ -11,10 +11,28 @@ const createTables = () =>{
       "PRIMARY KEY (`id`)" +
     ") ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;";
 
-  var tableName ="users";
+  
   connection.query(sqlQuery,function (err,result) {
       if (err) throw err;
+      let tableName ="users";
       console.log( tableName+ " Table created");
+  });
+
+  sqlQuery ="USE socialnetwork; CREATE TABLE IF NOT EXISTS `userinfo` (" +
+    "id INT NOT NULL ,"+
+    "username varchar(50) NOT NULL,"+
+    "profilepic varchar(255) NULL ,"+
+    "fullname VARCHAR(50) NULL,"+
+    "birthdate DATE NULL,"+
+    "bio VARCHAR(255) NULL,"+
+	  "PRIMARY KEY (`id`),"+
+    "FOREIGN KEY(`id`) REFERENCES users(id)"+
+  ")ENGINE = InnoDB AUTO_INCREMENT =2 DEFAULT CHARSET =utf8;";
+  
+  connection.query(sqlQuery,function (err,result) {
+    if (err) throw err;
+    let tableName ="userinfo";
+    console.log( tableName+ " Table created");
   });
 };
 
