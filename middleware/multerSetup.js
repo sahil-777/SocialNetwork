0956f32a848c75f09ee205aef2e65cba06ff69b5
sahil-777 <turbothunderboltz@gmail.com>
2021-02-05 03:02:08 +0530

    Added: Multer,middleware to read multipart/form-data

---
+++ middleware/multerSetup.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,'uploads/profilepics/');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+module.exports=upload;
