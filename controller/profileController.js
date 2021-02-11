fa17ebf7760f16fcc927eafb9350d15dcdd3e693
sahil-777 <turbothunderboltz@gmail.com>
2021-02-12 01:59:26 +0530

    Added: Show all uploaded feed on profile

--- controller/profileController.js
+++ controller/profileController.js
@@ -7,14 +7,19 @@ class profileController{
         //console.log(req.session.num);
         if(req.session.num!=null && typeof req.session.num!="undefined"){//If user has logged in
             console.log("LoggedIn "+req.session.num);
-            var sql="SELECT * FROM userinfo WHERE id = "+req.session.num;
+            let sql="SELECT * FROM userinfo WHERE id = "+req.session.num;
             connection.query(sql,(error,result)=>{
                 if(error) throw error;
                 console.log(result[0]);
                 var imageName=result[0].profilepic;
                 console.log(imageName);
                 if(typeof imageName=='object'){ imageName = "default_profilepic.png"; }
-                return res.render('profileView',{data:result[0],imageName:imageName});
+
+                let feedQuery="SELECT * FROM userfeed WHERE userid = "+req.session.num; 
+                connection.query(feedQuery,(error,feedResult)=>{
+                    console.log(feedResult);
+                    return res.render('profileView',{data:result[0],dpName:imageName,feedResult:feedResult});
+                })
             });
         }
         else{ 
