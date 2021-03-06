52e938d3edeef648c0afb8a5663e0f6f603e0fc9
sahil-777 <turbothunderboltz@gmail.com>
2021-03-06 18:34:03 +0530

    Fixed:Bug regarding load feed

--- controller/homeController.js
+++ controller/homeController.js
@@ -7,13 +7,17 @@ class homeController{
         //console.log(req.session.num);
         if(req.session.num!=null && typeof req.session.num!="undefined"){//If user has logged in
             console.log("LoggedIn "+req.session.num);
-            let feedQuery="SELECT username,feedname FROM userfeed WHERE userid!="+req.session.num;
+            let feedQuery="SELECT followinfo.following AS username,userfeed.feedname AS feedname FROM userfeed INNER JOIN followinfo "+ 
+             " ON userfeed.username = followinfo.following WHERE followinfo.follower= '"+req.session.username+"'"; 
+            //WHERE userid!="+req.session.num;
             //console.log(feedQuery);
-            connection.query(feedQuery,(error,feedResult)=>{
+             connection.query(feedQuery,(error,feedResult)=>{
+                if(error) throw error;
                 for(let i=0;i<feedResult.length;i++)
-                console.log(feedResult[i]);
-                return res.render('homeView',{feedResult:feedResult});
-            })
+                 console.log(feedResult[i]);
+            
+                 return res.render('homeView',{feedResult:feedResult});
+             });
         }
         else{ 
             console.log("Not loggedIn "+req.session.num);
