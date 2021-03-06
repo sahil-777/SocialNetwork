f78c7988b763185fe9e1376ac86c1509f51e4a30
sahil-777 <turbothunderboltz@gmail.com>
2021-03-06 16:28:07 +0530

    Updated: load feed is done , but without AJAX

--- controller/homeController.js
+++ controller/homeController.js
@@ -7,8 +7,13 @@ class homeController{
         //console.log(req.session.num);
         if(req.session.num!=null && typeof req.session.num!="undefined"){//If user has logged in
             console.log("LoggedIn "+req.session.num);
-            //let feedQuery="SELECT "
-            return res.render('homeView');
+            let feedQuery="SELECT username,feedname FROM userfeed WHERE userid!="+req.session.num;
+            //console.log(feedQuery);
+            connection.query(feedQuery,(error,feedResult)=>{
+                for(let i=0;i<feedResult.length;i++)
+                console.log(feedResult[i]);
+                return res.render('homeView',{feedResult:feedResult});
+            })
         }
         else{ 
             console.log("Not loggedIn "+req.session.num);
